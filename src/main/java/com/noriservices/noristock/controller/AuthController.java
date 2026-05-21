70a51a5da183617285ffe5edd0454fff27ce4ab4
jefflennon1 <jeffersonlennoncastro@gmail.com>
2026-05-21 13:50:02 -0300

    Enble basic setting from string security

--- src/main/java/com/noriservices/noristock/controller/AuthController.java
+++ src/main/java/com/noriservices/noristock/controller/AuthController.java
@@ -1,10 +1,17 @@
 package com.noriservices.noristock.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(name = "auth")
+@RequestMapping("auth")
 public class AuthController {
 
+
+    @GetMapping("/hello")
+    public ResponseEntity hello(){
+        return ResponseEntity.ok().body("Hello");
+    }
 }
---
+++ src/main/java/com/noriservices/noristock/service/JwtService.java
@@ -0,0 +1,14 @@
+package com.noriservices.noristock.service;
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+public class JwtService  {
+
+
+    public String generateToken(UserDetails user){
+
+        return"";
+    }
+}

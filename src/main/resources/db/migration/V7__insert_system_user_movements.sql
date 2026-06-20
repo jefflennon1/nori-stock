2cabb13e583deb0a36cb2615ad5a9596c8cfc3c6
jefflennon1 <jeffersonlennoncastro@gmail.com>
2026-06-20 11:40:30 -0300

    Fix some configs: Kafka consumer group-id and contraint CK_USERS_ROLE for insert default user into table users

--- src/main/resources/db/migration/V7__insert_system_user_movements.sql
+++ src/main/resources/db/migration/V7__insert_system_user_movements.sql
@@ -1,3 +1,9 @@
+ALTER TABLE users DROP CONSTRAINT ck_users_role;
+
+ALTER TABLE users ADD CONSTRAINT ck_users_role
+    CHECK (role IN ('OPERATOR', 'ADMIN', 'SYSTEM'));
+
+
 INSERT INTO users (id, name, username, email, password, role, active) VALUES
     ('00000000-0000-0000-0000-000000000001',
      'System',
